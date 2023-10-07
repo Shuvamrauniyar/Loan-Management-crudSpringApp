b2ae9e3e381563ac19f6f2fc23b8c8b0867fe278
Shuvam Rauniyar <shubhamrauniyar48@gmail.com>
2023-10-07 20:41:54 +0530

    created apply/create loan api

--- src/main/java/com/springboot/HSBCLoanManagement/controller/LoanDetailController.java
+++ src/main/java/com/springboot/HSBCLoanManagement/controller/LoanDetailController.java
@@ -1,5 +1,28 @@
 package com.springboot.HSBCLoanManagement.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springboot.HSBCLoanManagement.model.LoanDetails;
+import com.springboot.HSBCLoanManagement.service.LoanDetailService;
+
+
+@RestController
+@RequestMapping("/api")
 public class LoanDetailController {
 
+	private LoanDetailService loanDetailService;
+	
+	public LoanDetailController(LoanDetailService loanDetailService) {
+		this.loanDetailService = loanDetailService;
+	}
+	
+	@PostMapping("/applyLoan")
+	public ResponseEntity<LoanDetails> createLoanDetails(@RequestBody LoanDetails loan) {
+		return new ResponseEntity<LoanDetails>(loanDetailService.createLoanDetails(loan),HttpStatus.CREATED);
+	}
 }
--- src/main/java/com/springboot/HSBCLoanManagement/service/LoanDetailService.java
+++ src/main/java/com/springboot/HSBCLoanManagement/service/LoanDetailService.java
@@ -2,7 +2,20 @@ package com.springboot.HSBCLoanManagement.service;
 
 import org.springframework.stereotype.Service;
 
+import com.springboot.HSBCLoanManagement.model.LoanDetails;
+import com.springboot.HSBCLoanManagement.repository.LoanRepository;
+
 @Service
 public class LoanDetailService {
 	
+	private LoanRepository loanRepository;
+	
+	public LoanDetailService(LoanRepository loanRepository) {
+		this.loanRepository = loanRepository;
+	}
+	
+	public LoanDetails createLoanDetails(LoanDetails loanDetails) {
+		return loanRepository.save(loanDetails);
+		
+	}
 }
